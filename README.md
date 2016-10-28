2bdf8300cebf87a0d339462980996f0afeac7fb9
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-10-28 10:58:29 +0300

    simple webpack bundled project

---
+++ README.md
@@ -0,0 +1,9 @@
+#Angular 2 Ahead-of-Time compilation example.
+
+This is a simple application done in angular with webpack 2
+
+##How to run the application
+To start the example you must have [**node**](https://nodejs.org/en/download/) installed, then while in the project directory run in console these commands:
+
+    1.) npm install
+    2.) npm start
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/main.ts'
+    },
+    output: {
+        path: root('dist'),
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.html'],
+    },
+    module: {
+        rules: [
+            {test: /\.html$/, loader: 'raw'},
+            {test: /\.css$/, loader: 'raw'},
+            {test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']}
+        ]
+    },
+    devtool: '#source-map',
+};
+
+// Helpers
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [__dirname].concat(args));
+}
